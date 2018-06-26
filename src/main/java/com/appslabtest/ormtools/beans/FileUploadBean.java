0632825bbb40deaf30ae730c8d03c7536d1ada60
cyprian <cnwachukwu5@gmail.com>
2018-06-26 05:03:52 -0400

    Implementing fileUpload. Read rows, create KRI and add to ArrayList

--- src/main/java/com/appslabtest/ormtools/beans/FileUploadBean.java
+++ src/main/java/com/appslabtest/ormtools/beans/FileUploadBean.java
@@ -3,6 +3,9 @@ package com.appslabtest.ormtools.beans;
 
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 
@@ -18,6 +21,8 @@ import org.primefaces.model.UploadedFile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
+import com.appslabtest.ormtools.model.Department;
+import com.appslabtest.ormtools.model.KRI;
 import com.appslabtest.ormtools.utilities.MessengerUtil;
 
 
@@ -39,38 +44,60 @@ public class FileUploadBean implements Serializable {
 	}
 
 	public void handleKRIUpload(FileUploadEvent event) {
+		List<KRI> newKRIs = new ArrayList<>();
 		
 		try(InputStream fileInputStream = event.getFile().getInputstream()){
 			Workbook workbook = new HSSFWorkbook(fileInputStream);
 			Sheet sheet1 = workbook.getSheetAt(0);
-			//Iterator<Row> iterator = sheet1.iterator();
 			
 			int numRows = sheet1.getPhysicalNumberOfRows();
 			
-			for(int i = 0; i < numRows; i++) {
-				Row nextRow = sheet1.getRow(i);
-				int numCell = nextRow.getPhysicalNumberOfCells();
-				for(int j = 0; j < numCell; j++) {
-					Cell cell = nextRow.getCell(j);
+			//Get the first row and confirm that the columns headers are correct
+			Row headerRow = sheet1.getRow(0);
+			boolean validated = validateHeaderRow(headerRow);
+			
+			if(validated) {
+				for(int i = 1; i < numRows; i++) {
+					Row nextRow = sheet1.getRow(i);
+					KRI newKRI = new KRI();
+					int numCell = nextRow.getPhysicalNumberOfCells();
 					
-					//TODO
-					//Get the cell values for the first row. Check that they match the column names: "kri_desc", "kri_reason_collection", "kri_lower_bound", "kri_upper_bound" and "kri_owner_dept"
-					//Autowire KRIBean to access KRI instance and the addKRI method.
-					//Import KRI model class and instantiate it with each row of the excel data.
+					for(int j = 0; j < numCell; j++) {
+						Cell cell = nextRow.getCell(j);
+						if(cell != null) {
+							switch(getCellIndex(cell)) {
+								case 0:
+									newKRI.setKri_desc((String)getCellVaue(cell));	
+									break;
+								case 1:
+									newKRI.setKri_reason_for_collection((String)getCellVaue(cell));
+									break;
+								case 2:
+									newKRI.setKri_lower_bound((double)getCellVaue(cell));
+									break;
+								case 3:
+									newKRI.setKri_upper_bound((double)getCellVaue(cell));
+									break;
+								case 4:
+									Department kri_owner_dept = new Department();
+									kri_owner_dept.setDept_name((String)getCellVaue(cell));
+									newKRI.setKri_owner_dept(kri_owner_dept);
+									break;
+							}
+						}
+					}
 					
-//					System.out.print(cell.getStringCellValue());
-//					System.out.print(" ");
-				}
+					newKRIs.add(newKRI);
+					
+				}//End of for - statement
+				/*TODO
+				 * 1. Auto wire kriBean. loop through the list, create kri_code and insert on the db.
+				 * 2. Display message after successful data insertion into the db
+				 */
+			}else {
+				new MessengerUtil().addMessage(FacesMessage.SEVERITY_ERROR,"Error processing file. Invalid column headers ", "XLS read");
 			}
 			
-//			while(iterator.hasNext()) {
-//				Row nextRow = iterator.next();
-//				Iterator<Cell> cellIterator = nextRow.cellIterator();
-//				while(cellIterator.hasNext()) {
-//					Cell nextCell = cellIterator.next();
-//					System.out.print(nextCell.getStringCellValue());
-//				}
-//			}
 			workbook.close();
 		}catch (Exception e) {
 			// TODO: handle exception
@@ -81,4 +108,55 @@ public class FileUploadBean implements Serializable {
 		//System.out.println("Uploaded File Name Is :: "+file.getFileName()+" :: Uploaded File Size :: "+file.getSize());
 		new MessengerUtil().addMessage(FacesMessage.SEVERITY_INFO,"Got the file: ", "XLS read");
 	}
+	
+	private Object getCellVaue(Cell cell) {
+		
+		switch(cell.getCellTypeEnum()) {
+			case STRING: 
+				return cell.getStringCellValue();
+			case NUMERIC: 
+				return cell.getNumericCellValue();
+		default:
+			break;	
+		}
+		return null;
+	}
+	
+	private int getCellIndex (Cell cell) {
+		
+		return cell.getColumnIndex();
+	}
+	
+	private boolean validateHeaderRow(Row firstRow) {
+		Iterator<Cell> cellIterator = firstRow.cellIterator();
+		String headerCell0 = ""; String headerCell1 = ""; String headerCell2 = ""; String headerCell3 = ""; String headerCell4 = "";
+		boolean validated = false;
+		
+		while(cellIterator.hasNext()) {
+			Cell nextCell = cellIterator.next();
+			switch(getCellIndex(nextCell)) {
+				case 0: 
+					headerCell0 = (String)getCellVaue(nextCell);
+					break;
+				case 1: 
+					headerCell1 = (String)getCellVaue(nextCell);
+					break;
+				case 2: 
+					headerCell2 = (String)getCellVaue(nextCell);
+					break;
+				case 3: 
+					headerCell3 = (String)getCellVaue(nextCell);
+					break;
+				case 4: 
+					headerCell4 = (String)getCellVaue(nextCell);
+					break;
+			}
+		}
+		
+		if(headerCell0.equals("kri_desc") && headerCell1.equals("kri_reason_collection") && headerCell2.equals("kri_lower_bound") && headerCell3.equals("kri_upper_bound") && headerCell4.equals("kri_owner_dept")) {
+			validated = true;
+		}
+		return validated;
+	}//End of method
+	
 }
